6bfa9d35748d2b0fa2eded3a494bf48e762364ad
Shaanabot <shaankhank325@gmail.com>
2026-02-11 23:20:30 +0500

    Update credits and description for group information bot

--- models/commands/0_SHAAN-GROUP-INFORMENTION.js
+++ models/commands/0_SHAAN-GROUP-INFORMENTION.js
@@ -4,8 +4,8 @@ module.exports.config = {
 	name: "groupinf",
 	version: "1.0.0", 
 	hasPermssion: 1,
-	credits: "ARIF BABU",
-	description: "THIS BOT WAS MADE BY MR ARIF BABU",
+	credits: "SHAAN BABU",
+	description: "THIS BOT WAS MADE BY MR SHAAN BABU",
 	commandCategory: "GROUP INFORMETION", 
 	usages: "PREFIX", 
 	cooldowns: 0,
@@ -40,7 +40,7 @@ module.exports.run = async function({ api, event, args }) {
 			var callback = () =>
 				api.sendMessage(
 					{
-						body: `🌺 GROUP NAME 𒁍 ${threadName}\n🌺 GROUP UID 𒁍 ${id}\n🌺 GROUP APPROVAL 𒁍 ${pd}\n🌺 GROUP EMOJI 𒁍 ${icon}\n🌺 TOTAL MEMBER 𒁍 ${threadMem}\n🌺 TOTAL MALE 𒁍 ${nam}\n🌺 TOTAL FEMALE 𒁍 ${nu}\n🌺TOTAL GROUP ADMIN 𒁍 ${qtv}\n🌺 TOTAL GROUP MESSAGES 𒁍 ${sl}\n\n════════════════════════ ❁\nBOT CREATER BY MR ARIF BABU 🙂✌️`,
+						body: `🌺 GROUP NAME 𒁍 ${threadName}\n🌺 GROUP UID 𒁍 ${id}\n🌺 GROUP APPROVAL 𒁍 ${pd}\n🌺 GROUP EMOJI 𒁍 ${icon}\n🌺 TOTAL MEMBER 𒁍 ${threadMem}\n🌺 TOTAL MALE 𒁍 ${nam}\n🌺 TOTAL FEMALE 𒁍 ${nu}\n🌺TOTAL GROUP ADMIN 𒁍 ${qtv}\n🌺 TOTAL GROUP MESSAGES 𒁍 ${sl}\n\n════════════════════════ ❁\nBOT CREATER BY MR SHAAN BABU 🙂✌️`,
 						attachment: fs.createReadStream(__dirname + '/cache/1.png')
 					},
 					event.threadID,
